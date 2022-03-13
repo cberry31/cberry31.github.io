536139c48028f21f7c13a814101e5cdc519f0e06
cberry31 <coltonberry7@gmail.com>
2022-03-12 18:48:35 -0800

    Finished the homepage

--- components/navbar.js
+++ components/navbar.js
@@ -6,7 +6,7 @@ class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `<nav class="navbar sticky-top navbar-expand-lg navbar-dark bg-dark ">
         <div class="container-fluid ">
-            <a class="navbar-brand" href="#">Colton Berry</a>
+            <a class="navbar-brand" href="/">Colton Berry</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                 aria-expanded="false" aria-label="Toggle navigation">
@@ -15,7 +15,7 @@ class Header extends HTMLElement {
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.html">Home</a>
+                        <a class="nav-link" aria-current="page" href="/">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="projects.html">Projects</a>
